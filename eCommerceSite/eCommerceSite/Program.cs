fc9c2405458d40a70738238933a85cc193c60446
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2023-08-02 11:32:09 -0700

    Added exception filter

--- eCommerceSite/eCommerceSite/Program.cs
+++ eCommerceSite/eCommerceSite/Program.cs
@@ -4,9 +4,12 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<VideoGameContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
