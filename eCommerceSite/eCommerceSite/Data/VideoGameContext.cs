a25e05dfd6ea5e69a02a9163aa99f4385b526fbe
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2023-08-02 11:29:55 -0700

    Added default connection string

--- eCommerceSite/eCommerceSite/Data/VideoGameContext.cs
+++ eCommerceSite/eCommerceSite/Data/VideoGameContext.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace eCommerceSite.Data
 {
     public class VideoGameContext : DbContext
     {
         public VideoGameContext(DbContextOptions<VideoGameContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Game> Games { get; set; }
     }
 }
