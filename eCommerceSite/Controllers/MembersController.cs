c86cb6f7c13615e35f4ba8007bd26686634dd3ef
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2023-08-29 16:35:55 -0700

    Made Member Phone and Username optional

--- eCommerceSite/Controllers/MembersController.cs
+++ eCommerceSite/Controllers/MembersController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerceSite.Controllers
 {
     public class MembersController : Controller
     {
+        private readonly VideoGameContext _context;
+
+        public MembersController(VideoGameContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel regModel) 
+        {
+            if (ModelState.IsValid)
+            {
+                //Map RegisterViewModel to member
+                Member newMember = new()
+                {
+                    Email = regModel.Email,
+                    Password = regModel.Password
+                };
+
+                _context.Members.Add(newMember);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View(regModel);
         }
     }
 }
