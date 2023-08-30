e6185c30e8f708230ff53b42a030177b25519b50
TortillaWrappedParrot <119008633+TortillaWrappedParrot@users.noreply.github.com>
2023-08-29 18:46:06 -0700

    Added GameCatalogViewModel

---
+++ eCommerceSite/Models/GameCatalogViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace eCommerceSite.Models
+{
+    public class GameCatalogViewModel
+    {
+        public GameCatalogViewModel(List<Game> games, int lastPage, int curPage) 
+        { 
+            Games = games;
+            LastPage = lastPage;
+            CurrentPage = curPage;
+        }
+
+        public List<Game> Games { get; private set; }
+
+        public int LastPage { get; private set;}
+
+        public int CurrentPage { get; private set;}
+    }
+}
